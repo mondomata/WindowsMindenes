98606e2271147d2fe8810d3ae6c2e4b1122d13ab
mondomata <69905843+mondomata@users.noreply.github.com>
2025-01-18 17:27:28 +0100

    Add files via upload

---
+++ TitusWindowsUtil.ps1
@@ -0,0 +1,5 @@
+Write-Output "Push Y so yes, to run of script."
+Write-Output "Nyomj Y-t, mint yes/igent a szkript lefutásának engedélyezéséhez."
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+Write-Output "START CHRIS TITUS WINDOWS UTILITY /VÁRJ KICSIT, INDUL A CHRIS TITUS-FÉLE WINDOWS HASZNOS ESZKÖZÖK TÁROLÓJA" 
+iwr -useb https://christitus.com/win | iex
