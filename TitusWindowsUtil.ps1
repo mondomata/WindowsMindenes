5cc7ecb7ffa211f0813bf977dc78248605c01508
mondomata <69905843+mondomata@users.noreply.github.com>
2025-04-13 14:55:03 +0200

    Update TitusWindowsUtil.ps1

--- TitusWindowsUtil.ps1
+++ TitusWindowsUtil.ps1
@@ -1,5 +1,6 @@
-Write-Output "Push [Y] so yes, to run of script.";
-Write-Output "Nyomj [Y] billentyut azaz yes a szkript lefutasanak engedelyezesehez.";
-Write-Output "...Some secs start the programm. Aztan par masodperc es elindul a program...";
+Write-Host "Nyomj Y-t, mint yes/igent majd nyomj Entert a szkript lefutasanak engedelyezeshez. Push Y so yes, to run of script.";
+Write-Host "Varj turelmesen, par masodperc es indul a program...";
+Write-Host "-----------------------------------------";
+
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
 iwr -useb https://christitus.com/win | iex
