4c15a86c73a3f6c7ae09899fd663725355ce5fc1
mondomata <69905843+mondomata@users.noreply.github.com>
2025-04-13 14:14:42 +0200

    Update TitusWindowsUtil.ps1

--- TitusWindowsUtil.ps1
+++ TitusWindowsUtil.ps1
@@ -1,5 +1,5 @@
-Write-Output "Push Y so yes, to run of script."
-Write-Output "Nyomj Y-t, mint yes/igent a szkript lefutásának engedélyezéséhez."
+Write-Output "Push Y so yes, to run of script.";
+Write-Output "Nyomj Y-t, mint yes/igent a szkript lefutásának engedélyezéséhez.";
+Write-Output "...Aztan par masodperc es elindul a program...";
 Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
-Write-Output "START CHRIS TITUS WINDOWS UTILITY /VÁRJ KICSIT, INDUL A CHRIS TITUS-FÉLE WINDOWS HASZNOS ESZKÖZÖK TÁROLÓJA" 
 iwr -useb https://christitus.com/win | iex
